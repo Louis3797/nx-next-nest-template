d0fc25e0b66aecdd1ee3e82a866641287439cf4d
Louis <53270473+Louis3797@users.noreply.github.com>
2022-02-12 17:17:25 +0100

    setup

---
+++ libs/ui/src/lib/ui.tsx
@@ -0,0 +1,14 @@
+import './ui.module.scss';
+
+/* eslint-disable-next-line */
+export interface UiProps {}
+
+export function Ui(props: UiProps) {
+  return (
+    <div>
+      <h1>Welcome to Ui!</h1>
+    </div>
+  );
+}
+
+export default Ui;
